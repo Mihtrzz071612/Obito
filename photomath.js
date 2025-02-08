dc0852d15a6129f5e8f15c1770977ee24f2fd495
Minh Tran <mihtrzz2007@gmail.com>
2025-02-08 21:34:52 +0700

    Create photomath.js

---
+++ photomath.js
@@ -0,0 +1,16 @@
+var body = JSON.parse($response.body);
+
+body.content.tier.level = "genius";
+body.content.subscription = {
+      "packageName": "com.microblink.PhotoMath",
+      "productId": "com.microblink.PhotoMath.purchase.genius.one_year",
+      "orderId": "161207301107309",
+      "expiry": "3009-09-30T16:12:07Z"",
+      "started": "2023-09-30T16:12:07Z"",
+      "autoRenewing": true,
+      "status": "active",
+      "inGracePeriod": false,
+      "isTrial": false
+    };
+
+$done({ body: JSON.stringify(body) });
