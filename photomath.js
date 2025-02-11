e87ddbf16cd0f8d37a0820ca492a702d07ce32a9
Minh Tran <mihtrzz2007@gmail.com>
2025-02-11 21:42:03 +0700

    Update photomath.js

--- photomath.js
+++ photomath.js
@@ -1,5 +1,4 @@
 var body = JSON.parse($response.body);
-
 body.content.tier.level = "genius";
 body.content.subscription = {
       "packageName": "com.microblink.PhotoMath",
@@ -12,5 +11,4 @@ body.content.subscription = {
       "inGracePeriod": false,
       "isTrial": false
     };
-
 $done({ body: JSON.stringify(body) });
